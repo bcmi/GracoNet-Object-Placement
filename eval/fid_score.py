459f56785706d069e9279a052183f561a13da457
zsy <ysz58717@126.com>
2022-07-19 13:39:40 +0800

    Initial Commit

--- eval/fid_score.py
+++ eval/fid_score.py
@@ -49,7 +49,7 @@ except ImportError:
     def tqdm(x):
         return x
 
-from .inception import InceptionV3
+from inception import InceptionV3
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--batch-size', type=int, default=50,
